9188812f6274fd3165ffef6eb441e5ca7e7f521c
Akash <aakash23kriplani@gmail.com>
2022-02-07 16:19:30 +0530

    Add login logout and protected routes

--- build/routes/loginRoutes.js
+++ build/routes/loginRoutes.js
@@ -2,6 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
 const express_1 = require("express");
+function requireAuth(req, res, next) {
+    var _a;
+    if ((_a = req.session) === null || _a === void 0 ? void 0 : _a.loggedIn) {
+        next();
+        return;
+    }
+    res.status(403).send('Not permitted');
+}
 const router = (0, express_1.Router)();
 exports.router = router;
 router.get('/login', (req, res) => {
@@ -28,4 +36,30 @@ router.post('/login', (req, res) => {
     else {
         res.send('Invalid email or password');
     }
+});
+router.get('/', (req, res) => {
+    var _a;
+    if ((_a = req.session) === null || _a === void 0 ? void 0 : _a.loggedIn) {
+        res.send(`
+      <div>
+        You are logged in
+      </div>
+      <a href="/logout">Logout</a>
+    `);
+    }
+    else {
+        res.send(`
+      <div>
+        You are not logged in
+      </div>
+      <a href="/login">Login</a>
+    `);
+    }
+});
+router.get('/logout', (req, res) => {
+    req.session = undefined;
+    res.redirect('/');
+});
+router.get('/protected', requireAuth, (req, res) => {
+    res.send('Welcome to the protected route, logged in user.');
 });
